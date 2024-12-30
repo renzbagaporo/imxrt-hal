c7112f9c0eb3f4e9421c07e144cd1215e49a0824
renzbagaporo <renz.bagaporo@gmail.com>
2024-12-30 21:47:58 +0900

    Add build and flash script

---
+++ build.sh
@@ -0,0 +1,2 @@
+
+cargo build --example=hal_led --features=board/imxrt1010evk --target=thumbv7em-none-eabihf
---
+++ flash.sh
@@ -0,0 +1,1 @@
+cargo embed --example=hal_led --features=board/imxrt1010evk --target=thumbv7em-none-eabihf --chip=MIMXRT1010
