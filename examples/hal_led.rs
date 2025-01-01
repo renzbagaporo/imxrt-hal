07748780b2d0fa836c096f306bf072035d8b5f78
renzbagaporo <renz.bagaporo@gmail.com>
2025-01-01 11:08:41 +0900

    Blink LED

--- examples/hal_led.rs
+++ examples/hal_led.rs
@@ -3,10 +3,19 @@
 #![no_main]
 #![no_std]
 
+
 #[imxrt_rt::entry]
 fn main() -> ! {
     let (_, board::Specifics { led, .. }) = board::new();
+    let mut on = false;
     loop {
-        led.set();
+        on = !on;
+        if on {
+            led.set();
+        } else {
+            led.clear();
+        }
+
+        cortex_m::asm::delay(1000000000);
     }
 }
